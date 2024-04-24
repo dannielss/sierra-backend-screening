09d8344289d43e3d72594d2ebcf3b66dc986d321
dannielss <daniel_sousa.p@outlook.com>
2024-04-24 13:38:06 -0300

    feat: create all 3 routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
-const PORT = 8080;
 
 interface Listing {
   id: string;
@@ -9,4 +8,37 @@ interface Listing {
   description: string;
 }
 
-app.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+let listings: Listing[] = [];
+
+app.use(express.json());
+
+app.post("/listings", (req: Request, res: Response) => {
+  const body = req.body;
+  if (!body.id || !body.title || !body.description || !body.price) {
+    return res
+      .status(400)
+      .json({ error: "You should pass the correct data to create a listing" });
+  }
+
+  const { id, title, description, price } = body;
+
+  listings.push({ id, title, description, price });
+
+  return res.json({ message: "Listing created sucessfully" });
+});
+
+app.get("/listings", (_, res: Response) => {
+  return res.json({ listings });
+});
+
+app.delete("/listings/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const listingsFiltered = listings.filter((listing) => listing.id !== id);
+
+  listings = listingsFiltered;
+
+  return res.json({ message: "Listing deleted sucessfully" });
+});
+
+export { app };
